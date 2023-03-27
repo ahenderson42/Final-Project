ce60fa30195a5ade533c7b32818d438a85fa2c0e
Alex Henderson <ahenderson42@academic.rrc.ca>
2023-03-27 11:34:43 -0500

    Added association between models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :shopper
+
+  has_many :product_orders
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
   belongs_to :company
+
+  has_many :product_orders
 end
